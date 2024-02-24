8e63639864ca38533588a068ccc54a351e466de2
newskembot <136149248+newskembot@users.noreply.github.com>
2024-02-24 07:33:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/newskembot/QueeMusic",
+    "https://github.com/newskembot/Ankitvcplayerxyz",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
